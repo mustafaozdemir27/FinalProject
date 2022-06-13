0fab96cc8683fcfe6ef192941bbdd2df5be63422
mustafaozdemir27 <42694496+mustafaozdemir27@users.noreply.github.com>
2022-06-13 12:19:36 +0300

    Yazılım Geliştirici Yetiştirme Kampı 8. Gün C# Entity Framework

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+// SOLID 
+// Open Closed Principle
+ProductManager productManager = new ProductManager(new EfProductDal());
+Console.WriteLine("Tum Productlar");
+Console.WriteLine();
+foreach (var product in productManager.GetAll())
+{
+    Console.WriteLine($"ProductName ={product.ProductName}");
+}
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("Kategoriye gore productlar");
+Console.WriteLine();
+foreach (var product in productManager.GetAllByCategoryId(1))
+{
+    Console.WriteLine($"ProductName ={product.ProductName}");
+}
+
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("UnitPrice'a productlar");
+Console.WriteLine();
+foreach (var product in productManager.GetByUnitPrice(50,100))
+{
+    Console.WriteLine($"ProductName ={product.ProductName}");
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal : IEntityRepository<Product>
+    {
+        // Interface methodları default olarak public'tir ama kendisi değil.
+
+
+    }
+}
