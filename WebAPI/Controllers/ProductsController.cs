3650a78b6b697a797f296031e2cf877a15683760
mustafaozdemir27 <42694496+mustafaozdemir27@users.noreply.github.com>
2022-06-26 18:09:55 +0300

    Yazılım Geliştirici Yetiştirme Kampı 11. Gün C# Kurumsal Yazılım Mimarileri (1. Ders Sonu)

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] // <-- ATTRIBUTE BIR CLASS ILE ILGILI BILGI VERME IMZA VERME YONTEMIDIR
+
+    public class ProductsController : ControllerBase
+    {
+        // Loosely coupled
+        // naming convention
+        // IoC Container -- Inversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public List<Product> Get()
+        {
+
+            // Dependency chain --
+            //IProductService productService = new ProductManager(new EfProductDal());
+            var result = _productService.GetAll();
+            return _productService.GetAll().Data;
+ 
+        }
+    }
+}
