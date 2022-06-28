1d96e76facef968811dfe5d07cfbf3620d5bff8f
mustafaozdemir27 <42694496+mustafaozdemir27@users.noreply.github.com>
2022-06-28 14:12:55 +0300

    Yazılım Geliştirici Yetiştirme Kampı 11. Gün C# Kurumsal Yazılım Mimarileri Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -24,7 +24,7 @@ namespace Business.Concrete
         {
             // İş kodları
             // Yetkisi var mı?
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 20)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -21,15 +21,45 @@ namespace WebAPI.Controllers
         {
             _productService = productService;
         }
-        [HttpGet]
-        public List<Product> Get()
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
 
+            // Swagger
             // Dependency chain --
-            //IProductService productService = new ProductManager(new EfProductDal());
+            // IProductService productService = new ProductManager(new EfProductDal());
             var result = _productService.GetAll();
-            return _productService.GetAll().Data;
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
  
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return NotFound(result);
+        }
+
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
     }
 }
