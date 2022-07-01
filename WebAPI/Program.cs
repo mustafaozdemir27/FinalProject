5f044b95b887dcc15dc71900cea52ad2e1578495
mustafaozdemir27 <42694496+mustafaozdemir27@users.noreply.github.com>
2022-07-01 11:05:25 +0300

    Yazılım Geliştirici Yetiştirme Kampı 12. Gün C# Kurumsal Yazılım Mimarileri (Ilk Ders)

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -7,17 +10,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+
 builder.Services.AddControllers();
 // Icerisinde data tutulmuyorsa singleton kullanilir. Eger data tutuluyorsa kullanilmaz cunku herkese ayni instance verir. Diyelim e ticaret uygulamamiz var, sepeti goruntulemek istedik,
 // sepetteki urunler karisir cunku ayni instancelari veriyor.
 // AOP
 // Autofac bize AOP imkani sunuyor
 // Autofac, Ninject, CastleWindsor, StructureMap LightInject, DryInject --> IoC Container
-builder.Services.AddSingleton<IProductService, ProductManager>();
-builder.Services.AddSingleton<IProductDal,EfProductDal>();
+//builder.Services.AddSingleton<IProductService, ProductManager>();
+//builder.Services.AddSingleton<IProductDal,EfProductDal>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Call UseServiceProviderFactory on the Host sub property 
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new AutofacBusinessModule());
+});
 
 var app = builder.Build();
 
